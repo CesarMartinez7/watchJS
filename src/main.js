e9e90fd096d120a0cf93b03b4ee51175358477f4
CesarMartinez <cesarwamartinez@gmail.com>
2024-12-02 21:14:39 -0500

    se-añade-cronometro

--- src/main.js
+++ src/main.js
@@ -3,19 +3,19 @@ import javascriptLogo from "./javascript.svg";
 import viteLogo from "/vite.svg";
 import { setupCounter } from "./counter.js";
 import { format } from "date-fns";
+import githublogo from "./github-dark.svg"
 
-document.title = "TimeDate"
+document.title = "TimeDate";
 
 const ObtenerDate = () => {
-  const ahora = new Date()
-  return (format(ahora,"yyyy-MM-dd")) 
-}
+  const ahora = new Date();
+  return format(ahora, "yyyy-MM-dd");
+};
 
 const ObtenerHora = () => {
   const ahora = new Date();
   return format(ahora, "HH:mm:ss");
 };
-
 
 const mostrarDate = () => {
   const fechaElement = document.getElementById("hora");
@@ -23,32 +23,59 @@ const mostrarDate = () => {
 };
 
 setInterval(mostrarDate, 1000);
-setInterval(ObtenerDate,1000)
-
-
+setInterval(ObtenerDate, 1000);
 
 document.querySelector("#app").innerHTML = `
 <div>
+<a href="https://github.com/CesarMartinez7" target="_ublank">
+<img class="git" src=${githublogo}>
+</a>
+<h1>DateTime</h1>
 <p id="hora" class="hora" data-color:"#0F172A">${ObtenerHora()}</p>
 <p id="fecha" class="fecha">${ObtenerDate()}</p>
 </p>
-<a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-<img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
 </a>
 </div>
+<main class="cronometro">
+  <p class="hora">
+      <span id="minutos"></span>:<span id="segundos"></span>
+  </p>
+  <button id="start">Start</button>
+  <button id="stop">Stop</button>
+</main>
 `;
 
+const horaElement = document.getElementById("hora");
 
-const horaElement = document.getElementById("hora")
+console.log(horaElement);
 
-console.log(horaElement)
+horaElement.addEventListener("mouseenter", (e) => {
+  e.target.style.filter = `drop-shadow(${"#ffff"} 3px 2px 40px)`;
+  e.target.transition = "1s all ";
+});
 
-horaElement.addEventListener("mouseenter",(e)=>{
-  e.target.style.filter = `drop-shadow(${"#ffff"} 3px 2px 40px)`
-  e.target.transition = "1s all "
-})
+horaElement.addEventListener("mouseleave", (e) => {
+  e.target.style.filter = ``;
+});
 
-horaElement.addEventListener("mouseleave",(e)=>{
-  e.target.style.filter = ``
-})
+const MinutosElement = document.getElementById("minutos");
+const SegundosElement = document.getElementById("segundos");
+const ButtonStart = document.getElementById("start");
 
+let Segundos = 0;
+let Minutos = 0;
+
+MinutosElement.textContent = Minutos;
+SegundosElement.textContent = Segundos;
+
+ButtonStart.addEventListener("click", () => {
+  Segundos += 1;
+  setInterval(() => {
+    SegundosElement.textContent = Segundos++;
+    if (Segundos === 59) {
+      Minutos = Minutos + 1;
+      MinutosElement.textContent = Minutos;
+      Segundos = 0;
+    }
+  }, 1000);
+});
